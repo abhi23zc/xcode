3bdcec9d1e50028cc2a4baea504aeb263c1f1dfc
abhi23zc <you@example.com>
2024-07-07 01:10:54 +0530

    Deployment 3

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBoBkTFj2EDu2xtsrFEVVv8atiS8KsYifI",
+  authDomain: "znote-cbae4.firebaseapp.com",
+  projectId: "znote-cbae4",
+  storageBucket: "znote-cbae4.appspot.com",
+  messagingSenderId: "3449843500",
+  appId: "1:3449843500:web:2f92aaf564b169c064c689"
 };
 
 const app = initializeApp(firebaseConfig);
